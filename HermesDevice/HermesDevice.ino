c9d5554f02a9e1075147576d82265123a30508a2
JamaJ1701 <jamajeong@gmail.com>
2022-05-03 12:19:01 +1000

    Demo hardware version. Has vibration control depending on strength

--- HermesDevice/HermesDevice.ino
+++ HermesDevice/HermesDevice.ino
@@ -1,30 +1,41 @@
 #include <WiFi.h>
 #include <WebSocketClient.h>
 
+// WIFI connection
 const char* ssid     = "Jamas";
 const char* password = "jamajama";
+WiFiClient client;
 
-// This is the ip of the host. I personally set static ip for host.
+// Socket connection
+char thisID = '2';
 char host[] = "192.168.142.53";
-
-// Port used for connection
 int port = 5050;
-
 WebSocketClient webSocketClient;
 
-// Use WiFiClient class to create TCP connections
-WiFiClient client;
-char thisID = '2';
-int strength = 0;
+// Vibration related values
+const int pwmChannel = 0;
+const int pwmFreq = 1000;
+const int pwmResolution = 8; // 8-bit
+int signal_strength = 0; // vibration strength
+//int vib_strength = 0;
+int motorPin = 21; // motor control pin
+bool vibrating = false;
+int vib_freq = 0; // number of ticks the thing is on/off
+int freq_counter = 0;
+int prev_strength = 0;
+
+int connPin = 23;
+
+
 int updateStrength(){
   
   if (client.connect(host, port)) {
-    Serial.println("Connected");
+//    Serial.println("Connected");
+    digitalWrite(connPin, LOW);
   } else {
-    Serial.println("Connection failed.");
-    while(1) {
-      // Hang on failure
-    }
+//    Serial.println("Connection failed.");
+    digitalWrite(connPin, HIGH);
+    return '0';
   }
   
   client.write(thisID);
@@ -38,15 +49,39 @@ int updateStrength(){
       received = 1;
     }
   }
-  Serial.println(recvData);
+//  Serial.println(recvData);
   client.stop();
   return recvData;
+}
+void vibrate(){
+  if(vib_freq == 0){
+    vibrating = false;
+    ledcWrite(pwmChannel, 0);
+    return;
+  }
+  if(freq_counter == vib_freq){
+    vibrating = !vibrating;
+    freq_counter = 0;
+  } else{
+    freq_counter++;
+  }
+  if(vibrating){
+    ledcWrite(pwmChannel, 255);
+  } else{
+    ledcWrite(pwmChannel, 0);
+  }
+  return;
 }
 
 void setup() {
   Serial.begin(115200);
   delay(10);
 
+  ledcSetup(pwmChannel, pwmFreq, pwmResolution);
+  ledcAttachPin(motorPin, pwmChannel);
+  pinMode(connPin, OUTPUT);
+  
+  
   // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
@@ -67,13 +102,24 @@ void setup() {
   
 }
 
-
 void loop() {
+  
   char strengthChar;
   strengthChar = updateStrength();
-  Serial.println(strengthChar);
+  // change vibration strength based on strength
   
-  // wait to fully let the client disconnect
-  delay(1000);
+  int strength = strengthChar - '0';
+  Serial.println(strength);
+  if(prev_strength != strength){
+    prev_strength = strength;
+    if(strength == 0){
+      vib_freq = 0;
+    } else{
+      vib_freq = 4 - strength;
+    }
+    freq_counter = 0;
+  }
+  vibrate();
+  delay(200);
   
 }
